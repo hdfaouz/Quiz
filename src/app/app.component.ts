f9347bd8ac5a59c47bdac9dc1837bcbc244b9ddd
hdfaouz <hdfaouzia2@gmail.com>
2025-04-15 17:44:58 +0100

    Acceuil component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,22 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { NavbarComponent } from "./navbar/navbar.component";
+import { AcceuilComponent } from "./acceuil/acceuil.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, NavbarComponent],
+  imports: [RouterOutlet, NavbarComponent, AcceuilComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'QuizApp';
+  public questionsLimit : number;
+  public difficulty : string;
+
+  constructor(){
+    this.questionsLimit = 10;
+    this.difficulty = "Easy"
+  }
 }
